08b715466b19f4c0fb2bf7bda6cab443ffd3b6cc
Rahul <rahulchalat@yahoo.com>
2023-09-03 10:06:57 +0530

    h

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import Logo from '../assets/logo.png'
 const Navbar = () => {
   return (
     
     <nav className='flex items-center px-4  justify-between md:px-[5rem]  '>
       <Link to='/'>
-        <h1 className='font-bold  text-2xl p-4  '>Covid Tracker</h1>
+        <div className='flex items-center gap-0'>
+        <img className='h-16 w-16 ' src={Logo} alt="Logo" />
+        <h1 className='font-bold  text-2xl p-4  '>Covid Info</h1>
+        </div>
+       
       </Link>
    
    <ul className='flex items-center font-extrabold gap-4'> 
